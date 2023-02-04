8c3ed7f406a8aee627d6c96d07753c9e1d30dc4e
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-02-04 11:24:24 -0800

    04: refactor

--- 04/main.go
+++ 04/main.go
@@ -1,13 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
+
 	fmt.Println(getGreeting())
 }
 
 func getGreeting() string {
-	return fmt.Sprintf("Welcome to Go kit 0.12 Fundamentals!")
+	return "Welcome to Go kit 0.12 Fundamentals!"
 }
